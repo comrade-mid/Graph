2d1cc4dd718711edc25b0ade24ec0c0f832b1e42
comrade-mid <64811939+comrade-mid@users.noreply.github.com>
2021-11-30 15:19:59 -0800

    Test Unit for Graph ADT
    Bench test for the Graph ADT, no final commit or seen as gold unit testing.
    
    Simply add and subtract function tests as necessary for what functionality you're testing.

--- GraphTest.java
+++ GraphTest.java
@@ -10,32 +10,46 @@ public class GraphTest{
 		
 		G.addEdge(1,2);
 		G.addEdge(1,3);
-		G.addEdge(2,1);
 		G.addEdge(2,4);
 		G.addEdge(2,5);
 		G.addEdge(2,6);
-		G.addEdge(3,1);
 		G.addEdge(3,4);
-		G.addEdge(4,2);
-		G.addEdge(4,3);
 		G.addEdge(4,5);
-		G.addEdge(5,2);
-		G.addEdge(5,4);
 		G.addEdge(5,6);
-		G.addEdge(6,2);
-		G.addEdge(6,5);
 		
 		
-		G.BFS(6);
+		G.BFS(5);
 		
-	
-		
-		for(int i=0;i<6;i++){
-			System.out.println("Node #"+(i+1));
-			System.out.println("The node's parent is..." + G.getParent(i+1));
-			System.out.println("The node's distant from the source is... " + G.getDist(i+1));
+		for(int i=1;i<7;i++){
+			System.out.println("Node #"+i);
 			
+				System.out.print("The node's parent is... #"+G.getParent(i)+"\n");
+				System.out.print("The node's distance to source is... #"+G.getDist(i));
+			
+			System.out.print("\n");
 		}
+		
+		System.out.println("\n \n");
+		G.BFS(3);
+		
+		for(int i=1;i<7;i++){
+			System.out.println("Node #"+i);
+			
+				System.out.print("The node's parent is... #"+G.getParent(i)+"\n");
+				System.out.print("The node's distance to source is... #"+G.getDist(i));
+			
+			System.out.print("\n");
+		}
+		
+		
+		
+		System.out.print(G);
+		
+		List Test = new List();
+		System.out.println("Here's the current status of Test " + Test);
+		G.getPath(Test, 1);
+		
+		System.out.println("Here's the current status of Test " + Test);
 		
 	}
 	
